d9f9cda032ac5284091808463bbc8c6e111423aa
Navati Jain <navatijain@gmail.com>
2021-12-02 18:17:28 -0500

    init

---
+++ HorrorScopin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorrorScopin
+//
+//  Created by Navati Jain on 2021-12-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
