55f04c154083336f4a2534ceb4cda13a378af54a
Navati Jain <navatijain@gmail.com>
2021-12-03 15:48:49 -0500

    Sunsign picker

---
+++ HorrorScopin/SunSigns.swift
@@ -0,0 +1,12 @@
+//
+//  SunSigns.swift
+//  HorrorScopin
+//
+//  Created by Navati Jain on 2021-12-02.
+//
+
+import Foundation
+
+enum SunSigns: String, CaseIterable {
+    case aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn, aquarius, pisces
+}
--- HorrorScopin/ViewController.swift
+++ HorrorScopin/ViewController.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /* UI */
+    lazy var sunSignPicker: UIPickerView = {
+        let sunSignPicker = UIPickerView()
+        sunSignPicker.dataSource = self
+        sunSignPicker.delegate = self
+        sunSignPicker.translatesAutoresizingMaskIntoConstraints = false
+        return sunSignPicker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.addSubview(sunSignPicker)
+        sunSignPicker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        sunSignPicker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        sunSignPicker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
+}
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        SunSigns.allCases.count
+    }
+}
 
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        SunSigns.allCases[row].rawValue.capitalized
+    }
 }
 
