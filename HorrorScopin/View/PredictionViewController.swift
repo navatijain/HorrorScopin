7710039e7806389ba839583ecfcd12352e710f5a
Navati Jain <navatijain@gmail.com>
2021-12-08 05:53:10 -0500

    Started Prediction View controller and view model

--- HorrorScopin/View/PredictionViewController.swift
+++ HorrorScopin/View/PredictionViewController.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class PredictionViewController: UIViewController {
-
+    //MARK: UI
+    
+   lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Description:"
+        return label
+    }()
+    
+    lazy var descriptionLabelContents = UILabel()
+    
+    lazy var compatibilityLabel: UILabel = {
+         let label = UILabel()
+         label.text = "Compatibility:"
+         return label
+     }()
+     
+     lazy var compatibilityContents = UILabel()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [descriptionLabel,descriptionLabelContents,compatibilityLabel,compatibilityContents])
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
+        setup()
     }
+    
+    private func setup(){
+        view.addSubviewWithAutoLayout(stackView)
+        let constraints = stackView.anchor(to: view, with: UIEdgeInsets(top: 10, left: 10, bottom: -10, right: -10))
+        NSLayoutConstraint.activate(constraints)
+    }
+    
     
 
 
---
+++ HorrorScopin/ViewModel/PredictionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PredictionViewModel.swift
+//  HorrorScopin
+//
+//  Created by Navati Jain on 2021-12-08.
+//
+
+import Foundation
+
+class PredictionViewModel {
+    
+    
+}
